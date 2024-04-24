438822885b7a9bedfc430ea350fe3a44784df367
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-04-24 19:59:56 +0200

    Fixed styling on custom pages

--- admin/pages/navigations.jsx
+++ admin/pages/navigations.jsx
@@ -1,66 +1,38 @@
-/* eslint-disable react/no-unknown-property */
 /** @jsxRuntime classic */
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import Link from 'next/link';
 import { jsx } from '@keystone-ui/core';
-import { PageContainer } from '@keystone-6/core/admin-ui/components';
-import { Heading } from '@keystone-ui/core';
+import {
+  PageContainer,
+  CellContainer,
+  CellLink,
+} from '@keystone-6/core/admin-ui/components';
+import { Heading, Divider } from '@keystone-ui/core';
+import { FieldLabel, FieldDescription } from '@keystone-ui/fields';
 
 export default function Navigations() {
-  const containerStyle = {
-    height: '35px',
-    display: 'flex',
-    alignItems: 'center',
-    textAlign: 'left',
-    margin: 0,
-  };
-
-  const linkStyle = {
-    textDecoration: 'none',
-    color: '#6b7280',
-    fontWeight: '500',
-  };
-
-  const hoverStyle = {
-    color: '#3b82f6',
-    textDecoration: 'underline',
-    cursor: 'pointer',
-  };
-
   return (
-    <PageContainer header={<Heading type='h3'>Navigation configuration</Heading>}>
-      <h3
-        css={{
-          width: '100%',
-          textAlign: 'left',
-        }}
-      >
-        Manage site navigation
-      </h3>
+    <PageContainer
+      title='Navigation Configuration'
+      header={<Heading type='h3'>Manage site navigation</Heading>}
+    >
+      <CellContainer>
+        <FieldLabel>Choose the content you want to mange</FieldLabel>
+        <FieldDescription>
+          Main menu and Footer menu are the two main navigations that are required for the
+          site to function properly.
+          <br />
+          Main menu will be used in the top navigation and in the footer. <br />
+          Footer menu is placed beneath the Social Media links in the footer. <br />
+          Example of the footer menu: Cookies | Integrity policy
+        </FieldDescription>
 
-      <p
-        css={{
-          ...containerStyle,
-          backgroundColor: '#f3f4f6',
-          ':hover': { backgroundColor: '#eff6ff' },
-        }}
-      >
-        <Link css={{ ...linkStyle, ':hover': hoverStyle }} href='/main-menu/1'>
-          Main Menu
-        </Link>
-      </p>
-      <p
-        css={{
-          ...containerStyle,
-          ':hover': { backgroundColor: '#eff6ff' },
-        }}
-      >
-        <Link css={{ ...linkStyle, ':hover': hoverStyle }} href='/footer-menu/1'>
-          Footer Menu
-        </Link>
-      </p>
+        <CellLink href='/main-menu/1'>Main Menu</CellLink>
+        <Divider />
+        <CellLink href='/footer-menu/1'>Footer Menu</CellLink>
+        <Divider />
+      </CellContainer>
     </PageContainer>
   );
 }
--- admin/pages/site-config.jsx
+++ admin/pages/site-config.jsx
@@ -2,85 +2,38 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import Link from 'next/link';
 import { jsx } from '@keystone-ui/core';
-import { PageContainer } from '@keystone-6/core/admin-ui/components';
-import { Heading } from '@keystone-ui/core';
+import {
+  PageContainer,
+  CellContainer,
+  CellLink,
+} from '@keystone-6/core/admin-ui/components';
+import { Heading, Divider } from '@keystone-ui/core';
+import { FieldLabel, FieldDescription } from '@keystone-ui/fields';
 
 export default function SiteConfig() {
-  const containerStyle = {
-    height: '35px',
-    display: 'flex',
-    alignItems: 'center',
-    textAlign: 'left',
-    margin: 0,
-  };
-
-  const linkStyle = {
-    textDecoration: 'none',
-    color: '#6b7280',
-    fontWeight: '500',
-  };
-
-  const hoverStyle = {
-    color: '#3b82f6',
-    textDecoration: 'underline',
-    cursor: 'pointer',
-  };
-
   return (
-    <PageContainer header={<Heading type='h3'>Site configuration</Heading>}>
-      <h3
-        css={{
-          width: '100%',
-          textAlign: 'left',
-        }}
-      >
-        Manage site configuration
-      </h3>
+    <PageContainer
+      title='Site Configuration'
+      header={<Heading type='h3'>Manage Site configuration</Heading>}
+    >
+      <CellContainer>
+        <FieldLabel>Choose the content you want to mange</FieldLabel>
+        <FieldDescription>
+          The following content fields cannot be left empty. The site will not function
+          properly without them. <br />
+          For example, if no emails are provided in the form-email section, the forms will
+          not be able to send any emails.
+        </FieldDescription>
 
-      <p
-        css={{
-          ...containerStyle,
-          backgroundColor: '#f3f4f6',
-          ':hover': { backgroundColor: '#eff6ff' },
-        }}
-      >
-        <Link css={{ ...linkStyle, ':hover': hoverStyle }} href='/front-page/1'>
-          Frontpage
-        </Link>
-      </p>
-      <p
-        css={{
-          ...containerStyle,
-          ':hover': { backgroundColor: '#eff6ff' },
-        }}
-      >
-        <Link css={{ ...linkStyle, ':hover': hoverStyle }} href='/form-email/1'>
-          Forms
-        </Link>
-      </p>
-      <p
-        css={{
-          ...containerStyle,
-          backgroundColor: '#f3f4f6',
-          ':hover': { backgroundColor: '#eff6ff' },
-        }}
-      >
-        <Link css={{ ...linkStyle, ':hover': hoverStyle }} href='/footer-banner/1'>
-          Footer - Banner
-        </Link>
-      </p>
-      <p
-        css={{
-          ...containerStyle,
-          ':hover': { backgroundColor: '#eff6ff' },
-        }}
-      >
-        <Link css={{ ...linkStyle, ':hover': hoverStyle }} href='/footer-join-us/1'>
-          Footer - Join us
-        </Link>
-      </p>
+        <CellLink href='/front-page/1'>Frontpage</CellLink>
+        <Divider />
+        <CellLink href='/footer-banner/1'>Footer - Banner</CellLink>
+        <Divider />
+        <CellLink href='/form-email/1'>Forms - Email</CellLink>
+        <Divider />
+        <CellLink href='/footer-join-us/1'>Footer - Social Media Links</CellLink>
+      </CellContainer>
     </PageContainer>
   );
 }
