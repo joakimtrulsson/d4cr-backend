f0893ffc211d7c94830208bdf9852b6867ef55ef
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-05-10 12:08:29 +0200

    Updated AllSectionsField

--- customViews/fields/AllSectionsField.jsx
+++ customViews/fields/AllSectionsField.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { gql, useQuery } from '@keystone-6/core/admin-ui/apollo';
 import { FieldContainer, FieldLabel, FieldDescription } from '@keystone-ui/fields';
 import { AlertDialog } from '@keystone-ui/modals';
 import Modal from 'react-modal';
